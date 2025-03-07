b6da89729e591756b4191a263d2fb0f7ee392589
patravi <patric.ravi@gmail.com>
2025-03-07 19:20:06 +0530

    Completing the web app

--- web.py
+++ web.py
@@ -16,11 +16,9 @@ for index, todo in enumerate(todos):
         todos.pop(index)
         functions.write_todos(todos)
         del st.session_state[todo]
-        st.experimental_rerun()
+        st.rerun()
 
 st.text_input(label="Enter a todo", placeholder="Add a new todo...",
               on_change=add_todo, key="new_todo")
 
-print("Hello")
 
-st.session_state
